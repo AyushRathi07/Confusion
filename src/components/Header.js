41fa5bd06c6cf8ee116cbdb64b7f5c43d63c46f0
Ayush Rathi <ayushrathi982@gmail.com>
2020-05-22 13:42:33 +0530

    Uncontrolled form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Navbar, NavbarBrand, Jumbotron, Nav, Collapse, NavItem, NavbarToggler} from 'reactstrap';
+import {Navbar, NavbarBrand, Jumbotron, Nav, Collapse, NavItem, NavbarToggler, Modal, Label, Input, ModalHeader, ModalBody, Form, FormGroup, Button} from 'reactstrap';
 import {NavLink} from 'react-router-dom';
 class Header extends Component{
 
@@ -7,14 +7,30 @@ class Header extends Component{
         super(props);
         this.toggleNav = this.toggleNav.bind(this);
         this.state={
-            isNavOpen: false
+            isNavOpen: false,
+            isModalOpen: false
         }
+        this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
     }
 
     toggleNav(){
         this.setState({
             isNavOpen: !this.state.isNavOpen
-        })
+        });
+    }
+
+    toggleModal(){
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleLogin(event){
+        this.toggleModal();
+        alert("Username: "+this.username.value+", Password: "+this.password.value+", Remember: "+this.remember.checked);
+        event.preventDeault();
     }
 
     render(){
@@ -26,18 +42,23 @@ class Header extends Component{
                         <NavbarBrand className="mr-auto" href="/"><img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
-                            <NavItem>
-                                <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to='/aboutus'><span className="fa fa-info fa-lg"></span> About Us</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link"  to='/menu'><span className="fa fa-list fa-lg"></span> Menu</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
-                            </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to='/aboutus'><span className="fa fa-info fa-lg"></span> About Us</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link"  to='/menu'><span className="fa fa-list fa-lg"></span> Menu</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
+                                </NavItem>
+                            </Nav>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button onClick={this.toggleModal} className="bg-primary"><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                                </NavItem>
                             </Nav>
                         </Collapse>
                     </div>
@@ -52,6 +73,26 @@ class Header extends Component{
                         </div>
                     </div>
                 </Jumbotron>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label for="username">Username</Label>
+                                <Input type="text" name="username" innerRef={(input) => this.username = input}/>
+                            </FormGroup> 
+                            <FormGroup>
+                                <Label for="password">Password</Label>
+                                <Input type="password" name="password" innerRef={(input) => this.password = input}/>
+                            </FormGroup>
+                            <FormGroup check>
+                                <Input type="checkbox" name="remember" innerRef={(input) => this.remember = input}/>
+                                Remember me
+                            </FormGroup>
+                            <Button className="bg-dark mt-2">Submit</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </div>
         );
     }
